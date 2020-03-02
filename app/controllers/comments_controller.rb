e4ceb7acbdffe689eddcc6400022a954a465ac34
Jovan Stefanovski <jovan.stefanovski@webfactory.mk>
2020-03-02 17:59:12 +0100

    Fixed comment failed create/update routing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
           format.html { redirect_to @comment.idea, notice: 'Comment was successfully created.' }
           format.json { render :show, status: :created, location: @comment }
         else
-          format.html { render :new }
+          format.html { redirect_to @comment.idea, notice: 'Comment was not successfully created.' }
           format.json { render json: @comment.errors, status: :unprocessable_entity }
         end
       end
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
           format.html { redirect_to @comment.idea, notice: 'Comment was successfully updated.' }
           format.json { render :show, status: :ok, location: @comment }
         else
-          format.html { render :edit }
+          format.html { redirect_to @comment.idea, notice: 'Comment was not successfully updated.' }
           format.json { render json: @comment.errors, status: :unprocessable_entity }
         end
       end
